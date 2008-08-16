a0e713d52f4bbbc015ca0d2c3c09054922d8e14e
johntrammell <johntrammell@9d6afa6c-4954-0410-abbc-758f2e96163a>
2008-08-16 15:49:57 +0000

    added POD

--- trunk/lib/Algorithm/Voting/Ballot.pm
+++ trunk/lib/Algorithm/Voting/Ballot.pm
@@ -7,6 +7,25 @@ use base 'Class::Accessor::Fast';
 
 __PACKAGE__->mk_accessors(qw/ candidate /);
 
+=pod
+
+=head1 NAME
+
+Algorithm::Voting::Ballot - represents a ballot to cast in a race
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 Algorithm::Voting::Ballot->new()
+
+    # vote for Pedro
+    my $ballot = Algorithm::Voting::Ballot->new('Pedro')
+
+=cut
+
 sub new {
     my $class = shift;
     my $self = bless {}, $class;
@@ -15,7 +34,6 @@ sub new {
     }
     return $self;
 }
-
 
 1;
 
